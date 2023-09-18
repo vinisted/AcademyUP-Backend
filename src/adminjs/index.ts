602780b993a98dcd1fc8c0d6cad81a336ac8cf53
Vinicius Araújo de Figueiredo <54505830+vinisted@users.noreply.github.com>
2023-09-18 17:14:14 -0300

    configurando adminjs

---
+++ src/adminjs/index.ts
@@ -0,0 +1,34 @@
+import AdminJS from "adminjs";
+import AdminJSExpress from "@adminjs/express"
+import AdminJSSequelize from "@adminjs/sequelize"
+import { sequelize } from "../database"
+
+AdminJS.registerAdapter(AdminJSSequelize)
+
+export const adminJs = new AdminJS({
+    databases: [sequelize],
+    rootPath: "/admin",
+    branding: {
+        companyName: 'Academy Up',
+        logo: '/academyup-logo.svg',
+        theme: {
+          colors: {
+            primary100: '#fea000',
+              primary80: '#fea000',
+              primary60: '#fea000',
+              primary40: '#fea000',
+                primary20: '#fea000',
+              grey100: '#151515',
+              grey80: '#333333',
+              grey60: '#4d4d4d',
+              grey40: '#666666',
+              grey20: '#dddddd',
+              filterBg: '#333333',
+              accent: '#151515',
+              hoverBg: '#151515',
+            }
+        }
+    }
+})
+
+export const adminJsRouter = AdminJSExpress.buildRouter(adminJs)
