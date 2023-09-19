56e03890c23bfe453de2db24fc9e7d315867a8f1
Vinicius Araújo de Figueiredo <54505830+vinisted@users.noreply.github.com>
2023-09-19 20:21:18 -0300

    Inserindo novas tabelas e populando o banco e adicionando configuração de upload de video

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,11 +1,18 @@
+// src/models/index.ts
+
 import { Category } from './Category'
 import { Course } from './Course'
+import { Episode } from './Episode'
 
 Category.hasMany(Course)
 
 Course.belongsTo(Category)
+Course.hasMany(Episode)
+
+Episode.belongsTo(Course)
 
 export {
+  Category,
   Course,
-  Category
+  Episode
 }
