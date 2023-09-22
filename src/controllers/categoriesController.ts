c1a5c83f425c9043c9b124850f03e08445d1a6d6
Vinicius Araújo de Figueiredo <54505830+vinisted@users.noreply.github.com>
2023-09-22 04:13:47 -0300

    Adicionando rota de cursos de uma categoria

--- src/controllers/categoriesController.ts
+++ src/controllers/categoriesController.ts
@@ -17,5 +17,19 @@ export const categoriesController = {
         return res.status(400).json({ message: err.message })
       }
     }
+  },
+
+  // GET /categories/:id
+  show: async (req: Request, res: Response) => {
+    const { id } = req.params
+
+    try {
+      const category = await categoryService.findByIdWithCourses(id)
+      return res.json(category)
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
   }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -6,5 +6,6 @@ import { categoriesController } from './controllers/categoriesController'
 const router = express.Router()
 
 router.get('/categories', categoriesController.index)
+router.get('/categories/:id', categoriesController.show)
 
 export { router }
