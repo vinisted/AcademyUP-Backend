313c725190ac17d75022ea81a5c2f03b320d8839
Vinicius Araújo de Figueiredo <54505830+vinisted@users.noreply.github.com>
2023-09-22 05:25:40 -0300

    Adicionando rota que permite pesquisar cursos por nome

--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -1,5 +1,6 @@
 // src/services/courseService.ts
 
+import { Op } from "sequelize"
 import { Course } from "../models"
 
 export const courseService = {
@@ -54,5 +55,32 @@ export const courseService = {
         })
 
         return courses
+    },
+
+    findByName: async (name: string, page: number, perPage: number) => {
+        const offset = (page - 1) * perPage
+
+        const { count, rows } = await Course.findAndCountAll({
+            attributes: [
+                'id', 
+                'name', 
+                'synopsis', 
+                ['thumbnail_url', 'thumbnailUrl']
+            ],
+            where: {
+                name: {
+                    [Op.iLike]: `%${name}%`
+                }
+            },
+            limit: perPage,
+            offset
+        })
+
+        return {
+            courses: rows,
+            page,
+            perPage,
+            total: count
+        }
     }
 }
