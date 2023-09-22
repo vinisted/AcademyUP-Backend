e2763216d3425191b3db7e6f84370236748ed1e8
Vinicius Araújo de Figueiredo <54505830+vinisted@users.noreply.github.com>
2023-09-21 21:11:19 -0300

    Adicionando a primeira rota (categoria)

---
+++ src/routes.ts
@@ -0,0 +1,10 @@
+// src/routes.ts
+
+import express from 'express'
+import { categoriesController } from './controllers/categoriesController'
+
+const router = express.Router()
+
+router.get('/categories', categoriesController.index)
+
+export { router }
