a334552c14e61469b50fc4099964f3e29e9b5861
Vinicius Araújo de Figueiredo <54505830+vinisted@users.noreply.github.com>
2023-10-04 17:01:21 -0300

    Adicionando endpoint de likes nos cursos

--- src/models/index.ts
+++ src/models/index.ts
@@ -4,6 +4,7 @@ import { Category } from './Category'
 import { Course } from './Course'
 import { Episode } from './Episode'
 import { Favorite } from './Favorite'
+import { Like } from './Like'
 import { User } from './User'
 
 Category.hasMany(Course)
@@ -11,6 +12,7 @@ Category.hasMany(Course)
 Course.belongsTo(Category)
 Course.hasMany(Episode)
 Course.belongsToMany(User, { through: Favorite })
+Course.belongsToMany(User, { through: Like })
 Course.hasMany(Favorite, { as: 'favoritesUsers', foreignKey: 'course_id' })
 
 Episode.belongsTo(Course)
@@ -19,6 +21,7 @@ Favorite.belongsTo(Course)
 Favorite.belongsTo(User)
 
 User.belongsToMany(Course, { through: Favorite })
+User.belongsToMany(Course, { through: Like })
 User.hasMany(Favorite, { as: 'favoritesCourses', foreignKey: 'user_id' })
 
 export {
@@ -26,5 +29,6 @@ export {
   Course,
   Episode,
   Favorite,
+  Like,
   User
 }
